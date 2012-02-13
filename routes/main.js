15c5ca6483227da9940e09fd16bc8e27aaefc027
Paul Sham <contact@paulsham.com>
2012-02-13 09:36:04 -0500

    clean up and trying to get nav

--- routes/main.js
+++ routes/main.js
@@ -2,8 +2,9 @@
 /*
  * Main routes
  */
- 
+
 app.get('/', function(req, res){
+	
 	res.render('static/index', {
 		title: 'Welcome to Paul Sham\'s base project!',
 		route: req.socket.server.route  // Passes route to set current item in nav
